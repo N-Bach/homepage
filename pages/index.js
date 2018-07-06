5ccb78143c37547bb14083590a8af58e1db26375
NBach <nguyenbach2810@gmail.com>
2018-07-06 15:44:29 +0700

    First commit to beta

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Header from '@components/Header/Header'
 
 class Index extends Component {
   render () {
     return (
       <div>
+        <Header />
         This is index
       </div>
     )
