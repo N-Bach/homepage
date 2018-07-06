f73b66d83b4a15fe19c0f2947002328c1367101b
N-Bach <nguyenbach2810@gmail.com>
2018-07-06 15:02:21 +0700

    First commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        This is index
+      </div>
+    )
+  }
+}
+
+export default Index
